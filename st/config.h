58598a15406679dc26d199f8482f07336608693e
codehhr <1871973389@qq.com>
2020-08-10 16:30:28 +0800

    set  to close window

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "WenQuanYi Micro Hei Mono:pixelsize=22:antialias=true:autohint=true";
+static char *font = "WenQuanYi Micro Hei Mono:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 4;
 
 /*
