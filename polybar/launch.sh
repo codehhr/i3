d507360bb0452b7f2c49061d575df94643a250b5
codehhr <1871973389@qq.com>
2020-10-15 11:47:31 +0800

    _

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,4 +5,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar bar1
+polybar barA
