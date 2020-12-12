6e89b3d68d7ac5bf49325f67d4d25412291136ab
codehhr <1871973389@qq.com>
2020-12-12 15:58:58 +0800

    simpleTerminal

--- st/config.h
+++ st/config.h
@@ -171,8 +171,6 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ XK_NO_MOD,            Button4, kscrollup,      {.i = 1} },
-	{ XK_NO_MOD,            Button5, kscrolldown,    {.i = 1} },
 	{ ControlMask,          Button4, kscrollup,      {.i = -1} },
 	{ ControlMask,          Button5, kscrolldown,    {.i = -1} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
