94c68f079cec8927e87bddc267daf6fb871fa4fe
codehhr <1871973389@qq.com>
2021-07-24 18:45:46 +0800

    _

--- st/config.h
+++ st/config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "WenQuanYi Micro Hei Mono:pixelsize=22:antialias=true:autohint=true";
+// static char *font = "WenQuanYi Micro Hei Mono:pixelsize=22:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=22:antialias=true:autohint=true";
 // static int borderpx = 20;
 static int borderpx = 14;
 
