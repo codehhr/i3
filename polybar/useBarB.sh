c7aaa59fd02d838e0a73706551b24ac30d3c4379
codehhr <1871973389@qq.com>
2021-11-21 21:17:09 +0800

    add polybarB with config

---
+++ polybar/useBarB.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+rm ~/.config/polybar/config && cp ~/.config/polybar/barB_config ~/.config/polybar/config
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar barB
